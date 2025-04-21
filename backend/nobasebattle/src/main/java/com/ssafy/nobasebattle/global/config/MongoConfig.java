11a4899d03160cec13db6d18304d78a00f6b38a7
chanu2 <mdsoo55828@gmail.com>
2025-04-21 17:09:48 +0900

    feat: 몽고 db 설정

---
+++ backend/nobasebattle/src/main/java/com/ssafy/nobasebattle/global/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.ssafy.nobasebattle.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
