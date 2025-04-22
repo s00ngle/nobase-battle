edf851e25c23507fadc8a63b153ae67c7a48a379
상승규 <grandsword25@naver.com>
2025-04-22 17:45:04 +0900

    #S12P31B206-35 - feat : 임시 characterRepository 작성

--- backend/nobasebattle/src/main/java/com/ssafy/nobasebattle/domain/textcharacter/domain/repository/TextCharacterRepository.java
+++ backend/nobasebattle/src/main/java/com/ssafy/nobasebattle/domain/textcharacter/domain/repository/TextCharacterRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.nobasebattle.domain.textcharacter.domain.repository;
 
 import com.ssafy.nobasebattle.domain.textcharacter.domain.TextCharacter;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface TextCharacterRepository extends MongoRepository<TextCharacter, String> {
-
+    @Query("{ 'createdAt' : { $gte: ?0, $lt: ?1 } }")
+    List<TextCharacter> findByCreatedAtBetween(LocalDateTime createdAtAfter, LocalDateTime createdAtBefore);
 }
