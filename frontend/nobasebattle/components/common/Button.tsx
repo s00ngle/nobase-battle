bfdf38843a30e28a8c35b3e3c488c1b6c87732e4
김용순 <dydtns9815@gmail.com>
2025-04-23 11:10:30 +0900

    refactor: fill 옵션 추가 및 py-2로 조정

--- frontend/nobasebattle/components/common/Button.tsx
+++ frontend/nobasebattle/components/common/Button.tsx
@@ -1,23 +1,24 @@
-import { hover, transparentForm } from "@/styles/form";
+import { hover, transparentForm } from '@/styles/form'
 
 interface ButtonProps {
-  text: string;
-  border?: boolean;
-  onClick?: () => void;
+  text: string
+  border?: boolean
+  onClick?: () => void
+  fill?: boolean
 }
 
-const Button = ({ text, border, onClick }: ButtonProps) => {
+const Button = ({ text, border = false, onClick, fill = false }: ButtonProps) => {
   return (
     <button
       type="button"
       onClick={onClick}
-      className={`${
-        border ? "border border-1-white" : ""
-      } ${transparentForm} ${hover} px-3 py-3 rounded-lg text-xl cursor-pointer`}
+      className={`${border ? 'border border-1-white' : ''}
+      ${fill ? 'w-full' : ''}
+      ${transparentForm} ${hover} px-3 py-2 rounded-lg text-xl cursor-pointer`}
     >
       {text}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
