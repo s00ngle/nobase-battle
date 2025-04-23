845b867d897d49718a391d2a894a2abe228b7d08
김용순 <dydtns9815@gmail.com>
2025-04-23 14:39:32 +0900

    feat: 페이지 기본 생성

---
+++ frontend/nobasebattle/app/profile/page.tsx
@@ -0,0 +1,5 @@
+const ProfilePage = () => {
+  return <div className="text-5xl">Profile Page</div>
+}
+
+export default ProfilePage
---
+++ frontend/nobasebattle/app/rank/page.tsx
@@ -0,0 +1,5 @@
+const RankPage = () => {
+  return <div className="text-5xl">Rank Page</div>
+}
+
+export default RankPage
