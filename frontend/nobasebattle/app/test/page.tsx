99cf289dd1be48d2686ef47dabbb8bad41e1b66f
김용순 <dydtns9815@gmail.com>
2025-04-23 12:39:58 +0900

    feat: CharacterTypeToggle 개발

--- frontend/nobasebattle/app/test/page.tsx
+++ frontend/nobasebattle/app/test/page.tsx
@@ -1,7 +1,29 @@
+'use client'
+
+import CharacterTypeToggle from '@/components/character/CharacterTypeToggle'
+import { useState } from 'react'
+
+type CharacterType = 'text' | 'image'
+
 const TestPage = () => {
+  const [selectedType, setSelectedType] = useState<CharacterType>('text')
+
+  const handleClickText = () => {
+    setSelectedType('text')
+  }
+
+  const handleClickImage = () => {
+    setSelectedType('image')
+  }
+
   return (
     <div>
       <div className="text-5xl">Test Page</div>
+      <CharacterTypeToggle
+        selectedType={selectedType}
+        onChangeText={handleClickText}
+        onChangeImage={handleClickImage}
+      />
     </div>
   )
 }
--- frontend/nobasebattle/components/layout/Header.tsx
+++ frontend/nobasebattle/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggleButton from '../common/ThemeToggleButton'
 const Header = () => {
   return (
     <div className="flex p-3 absolute top-0">
-      <div className="text-4xl text-accent">너 개약하자나;</div>
+      <div className="text-4xl text-accent  whitespace-nowrap">너 개약하자나;</div>
       <div className="fixed right-4">
         <ThemeToggleButton />
       </div>
