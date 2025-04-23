4a0fe090e78a3567e84ccc6f39f13da5cb1fc8cc
김선명 <keemsnmyn@naver.com>
2025-04-23 12:43:07 +0900

    S12P31B206-50-feat: 가입 메인 페이지 개발 완료

--- frontend/nobasebattle/components/common/InputBox.tsx
+++ frontend/nobasebattle/components/common/InputBox.tsx
@@ -6,14 +6,15 @@ interface InputBoxProps {
   text?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
 }
 
-const InputBox = ({ text, value, onChange }: InputBoxProps) => {
+const InputBox = ({ text, value, onChange, type = "text" }: InputBoxProps) => {
   return (
     <div className="flex flex-col gap-2">
       <span className="text-sm">{text}</span>
       <input
-        type="text"
+        type={type}
         value={value}
         onChange={onChange}
         className={`${transparentForm} ${hover} w-full text-sm px-3 py-3 rounded-xl focus:outline-none focus:border-white border border-transparent`}
