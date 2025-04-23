f3c5e3740b2cdfadb1bb89a65922564fa5298182
김용순 <dydtns9815@gmail.com>
2025-04-23 11:01:22 +0900

    feat: Footer 컴포넌트 개발

--- frontend/nobasebattle/app/layout.tsx
+++ frontend/nobasebattle/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Footer from '@/components/layout/Footer'
 import Header from '@/components/layout/Header'
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
--- frontend/nobasebattle/components/layout/Footer.tsx
+++ frontend/nobasebattle/components/layout/Footer.tsx
@@ -1,3 +1,35 @@
-const Footer = () => {}
+import { hover, transparentForm } from '@/styles/form'
+import Link from 'next/link'
+
+interface OptionProps {
+  text: string
+  link: string
+}
+
+const Option = ({ text, link }: OptionProps) => {
+  return (
+    <Link href={link} className={`w-25 h-15 flex justify-center items-center ${hover}`}>
+      {text}
+    </Link>
+  )
+}
+
+const OptionList = [
+  { text: '홈', link: '/' },
+  { text: '투기장', link: '/event' },
+  { text: '랭킹', link: '/rank' },
+  { text: '프로필', link: '/profile' },
+  { text: '테스트', link: '/test' },
+]
+
+const Footer = () => {
+  return (
+    <div className={`flex w-full justify-center fixed bottom-0 ${transparentForm}`}>
+      {OptionList.map((option) => (
+        <Option key={option.link} text={option.text} link={option.link} />
+      ))}
+    </div>
+  )
+}
 
 export default Footer
