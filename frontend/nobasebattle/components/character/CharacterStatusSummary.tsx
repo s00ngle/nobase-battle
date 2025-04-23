f07d86d9f55ebaeb935cabb4af341bc5b6a86682
김용순 <dydtns9815@gmail.com>
2025-04-23 17:58:08 +0900

    hotfix: 캐릭터 정보 배경 추가 및 whitespace-nowrap 속성 추가

--- frontend/nobasebattle/components/character/CharacterStatusSummary.tsx
+++ frontend/nobasebattle/components/character/CharacterStatusSummary.tsx
@@ -1,9 +1,9 @@
-import { transparentForm } from "@/styles/form";
+import { transparentForm } from '@/styles/form'
 
 interface SummaryProps {
-  eloScore: number;
-  rank: number;
-  winRate: number;
+  eloScore: number
+  rank: number
+  winRate: number
 }
 
 const CharacterStatusSummary = ({ eloScore, rank, winRate }: SummaryProps) => {
@@ -11,24 +11,30 @@ const CharacterStatusSummary = ({ eloScore, rank, winRate }: SummaryProps) => {
     <div className="flex gap-2 w-full">
       <div className="flex flex-col items-center gap-2 text-base w-full">
         <span>레이팅</span>
-        <span className={`text-center w-full px-3 py-3 rounded-xl ${transparentForm}`}>
+        <span
+          className={`text-center w-full px-3 py-3 rounded-xl whitespace-nowrap ${transparentForm}`}
+        >
           {eloScore}점
         </span>
       </div>
       <div className="flex flex-col items-center gap-2 text-base w-full">
         <span>순위</span>
-        <span className={`text-center w-full px-3 py-3 rounded-xl ${transparentForm}`}>
+        <span
+          className={`text-center w-full px-3 py-3 rounded-xl whitespace-nowrap ${transparentForm}`}
+        >
           {rank}등
         </span>
       </div>
       <div className="flex flex-col items-center gap-2 text-base w-full">
         <span>승률</span>
-        <span className={`text-center w-full px-3 py-3 rounded-xl ${transparentForm}`}>
+        <span
+          className={`text-center w-full px-3 py-3 rounded-xl whitespace-nowrap ${transparentForm}`}
+        >
           {winRate}%
         </span>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CharacterStatusSummary;
+export default CharacterStatusSummary
