6f7332a49f1263dc2a6c23dbec50f8d13e2ed660
김용순 <dydtns9815@gmail.com>
2025-04-25 18:39:31 +0900

    feat: 정적 파일 요청 처리 로직 추가 및 미들웨어 경로 매처 개선

--- frontend/nobasebattle/middleware.ts
+++ frontend/nobasebattle/middleware.ts
@@ -6,6 +6,22 @@ export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   const token = request.cookies.get('token')
 
+  // 정적 파일 요청은 바로 통과
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/api') ||
+    pathname.includes('/public') ||
+    pathname.endsWith('.png') ||
+    pathname.endsWith('.jpg') ||
+    pathname.endsWith('.jpeg') ||
+    pathname.endsWith('.gif') ||
+    pathname.endsWith('.ico') ||
+    pathname.endsWith('.ttf') ||
+    pathname.endsWith('.otf')
+  ) {
+    return NextResponse.next()
+  }
+
   // 토큰이 없는 경우 /register로 리다이렉트 (단, /register 페이지는 제외)
   if (!token && pathname !== '/register') {
     return NextResponse.redirect(new URL('/register', request.url))
@@ -21,5 +37,13 @@ export async function middleware(request: NextRequest) {
 
 // 미들웨어가 적용될 경로 지정
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|public).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
 }
