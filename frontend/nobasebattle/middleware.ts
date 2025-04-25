45fd719362816f6fc968beb19813e8ddfe03d16c
김용순 <dydtns9815@gmail.com>
2025-04-25 18:20:11 +0900

    feat: 미들웨어 경로 매처 수정 및 CSS에서 폰트 관련 스타일 개선

--- frontend/nobasebattle/middleware.ts
+++ frontend/nobasebattle/middleware.ts
@@ -21,5 +21,5 @@ export async function middleware(request: NextRequest) {
 
 // 미들웨어가 적용될 경로 지정
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|public).*)'],
 }
