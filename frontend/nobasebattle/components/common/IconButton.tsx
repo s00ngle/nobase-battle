ae2a394fa745a6bb0988866627a754efaa43f93f
김용순 <dydtns9815@gmail.com>
2025-04-25 09:55:52 +0900

    fix: IconButton 컴포넌트에서 이미지 경로 수정

--- frontend/nobasebattle/components/common/IconButton.tsx
+++ frontend/nobasebattle/components/common/IconButton.tsx
@@ -8,7 +8,7 @@ interface IconButtonProps {
 const IconButton = ({ icon }: IconButtonProps) => {
   return (
     <div className={`p-1.5 rounded-md ${hover} w-fit cursor-pointer`}>
-      <Image src={icon} alt={icon} width={14} height={14} />
+      <Image src={`/${icon}`} alt={icon} width={14} height={14} />
     </div>
   )
 }
