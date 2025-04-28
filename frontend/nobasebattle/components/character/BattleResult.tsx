750964103ebb0216124382d5f20611f270da6c2d
김용순 <dydtns9815@gmail.com>
2025-04-28 13:56:55 +0900

    feat: 배틀 결과 표시 방식 개선 및 결과에 따른 색상 변경

--- frontend/nobasebattle/components/character/BattleResult.tsx
+++ frontend/nobasebattle/components/character/BattleResult.tsx
@@ -10,7 +10,23 @@ interface ResultProps {
 const BattleResult = ({ data }: ResultProps) => {
   return (
     <div className="flex flex-col gap-3">
-      <p className="text-xl">결과: {data.result}! </p>
+      <p className="text-xl">
+        결과:{' '}
+        <span
+          className={
+            data.result === 1 ? 'text-green-500' : data.result === -1 ? 'text-red-500' : ''
+          }
+        >
+          {data.result === 1
+            ? '승리'
+            : data.result === 0
+              ? '무승부'
+              : data.result === -1
+                ? '패배'
+                : ''}
+          !
+        </span>
+      </p>
       <div className={`flex flex-col ${transparentForm} w-full px-3 py-3 rounded-2xl gap-3`}>
         <span className="text-xl text-center">
           {data?.firstCharacter.name} vs {data?.secondCharacter.name}
