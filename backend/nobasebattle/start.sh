1c8d5a6bb496c8694ef576502204417c10bf0e1b
상승규 <grandsword25@naver.com>
2025-04-28 12:41:21 +0900

    feat: 배포용 파일

---
+++ backend/nobasebattle/start.sh
@@ -0,0 +1,9 @@
+./gradlew clean build -x test --no-daemon
+
+sudo docker stop nbb
+sudo docker rm nbb
+sudo docker image remove nobase
+
+sudo docker build -t nobase .
+
+sudo docker run -d --name nbb --env-file .env -p 8080:8080 --network test nobase
