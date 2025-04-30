5a50be4e9e376bb9637efb6baa70f837077eed2d
김용순 <dydtns9815@gmail.com>
2025-04-30 14:57:32 +0900

    feat: 랭킹 페이지의 버튼 스타일 개선 및 배지 목록 정렬 기능 추가

--- frontend/nobasebattle/components/ranking/RankingItem.tsx
+++ frontend/nobasebattle/components/ranking/RankingItem.tsx
@@ -1,13 +1,13 @@
-import BadgeList from "@/components/common/BadgeList";
-import { hover, transparentForm } from "@/styles/form";
-import type { BadgeType } from "@/types/Badge";
+import BadgeList from '@/components/common/BadgeList'
+import { hover, transparentForm } from '@/styles/form'
+import type { BadgeType } from '@/types/Badge'
 
 interface RankingItemsProps {
-  rank: number;
-  characterName: string;
-  username?: string;
-  eloScore: number;
-  badgeList?: BadgeType[];
+  rank: number
+  characterName: string
+  username?: string
+  eloScore: number
+  badgeList?: BadgeType[]
 }
 
 const RankingItems = ({
@@ -18,19 +18,16 @@ const RankingItems = ({
   badgeList = [],
 }: RankingItemsProps) => {
   return (
-    <div
-      className={`flex justify-between px-4 py-4 rounded-xl ${transparentForm} ${hover}`}
-    >
-      <div className="flex gap-7 items-center">
-        <span className="text-3xl w-9 text-center">{rank}</span>
-        <div className="flex flex-col gap-1">
-          <span className="text-xl max-sm:truncate max-sm:max-w-30">
-            {characterName}
-          </span>
-          <span className="text-sm">{username}</span>
+    <div className={`flex justify-between gap-2 px-4 py-4 rounded-xl ${transparentForm} ${hover}`}>
+      <div className="flex gap-4 items-center min-w-0">
+        <span className="text-3xl w-9 text-center flex-shrink-0">{rank}</span>
+        <div className="flex flex-col gap-1 min-w-0">
+          <span className="text-xl truncate whitespace-nowrap">{characterName}</span>
+          <span className="text-sm truncate whitespace-nowrap">{username}</span>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-end gap-1">
+      {/* 점수와 뱃지 목록 */}
+      <div className="flex flex-col justify-center items-end gap-1 flex-shrink-0">
         <span className="text-xl whitespace-nowrap">{eloScore}점</span>
         {badgeList && badgeList.length > 0 && (
           <BadgeList
@@ -38,11 +35,12 @@ const RankingItems = ({
             size={25}
             isTransparent={false}
             isPadding={false}
+            isRightAligned={true}
           />
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default RankingItems;
+export default RankingItems
