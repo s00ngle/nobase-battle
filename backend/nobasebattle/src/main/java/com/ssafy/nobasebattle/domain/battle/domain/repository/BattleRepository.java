8e5e4f007fd06282b6d02fcf38060e19f50eea1d
김찬우 <mdsoo55828@gmail.com>
2025-05-08 15:16:04 +0900

    Revert "Merge branch 'BE-feat/S12P31B206-22/valid' into 'master'"
    This reverts merge request !100

--- backend/nobasebattle/src/main/java/com/ssafy/nobasebattle/domain/battle/domain/repository/BattleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.nobasebattle.domain.battle.domain.repository;
-
-import com.ssafy.nobasebattle.domain.battle.domain.Battle;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BattleRepository extends MongoRepository<Battle, String>  {
-}
