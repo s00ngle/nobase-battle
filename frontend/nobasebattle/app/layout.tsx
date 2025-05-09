09efcb90a9c9f2b71b8559daffabd35544aa99ff
김선명 <keemsnmyn@naver.com>
2025-05-09 09:24:15 +0900

    chore: Google Analytics 추가

--- frontend/nobasebattle/app/layout.tsx
+++ frontend/nobasebattle/app/layout.tsx
@@ -1,38 +1,39 @@
-import type { Metadata } from 'next'
-import { ThemeProvider } from 'next-themes'
-import './globals.css'
-import Footer from '@/components/layout/Footer'
-import Header from '@/components/layout/Header'
-import Head from 'next/head'
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import Head from "next/head";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
-  title: '무근본 배틀',
-  description: '나만의 캐릭터를 만들어서 승리해 보세요.',
+  title: "무근본 배틀",
+  description: "나만의 캐릭터를 만들어서 승리해 보세요.",
   icons: {
-    icon: '/favicon.ico',
-    apple: '/favicon-apple.png',
+    icon: "/favicon.ico",
+    apple: "/favicon-apple.png",
   },
   openGraph: {
-    title: '무근본 배틀 - 나만의 캐릭터를 만들어서 승리해 보세요.',
-    description: '세계 최강! 프롬프팅 배틀',
-    url: 'https://nobasebattle.com/',
+    title: "무근본 배틀 - 나만의 캐릭터를 만들어서 승리해 보세요.",
+    description: "세계 최강! 프롬프팅 배틀",
+    url: "https://nobasebattle.com/",
     images: [
       {
-        url: 'https://nobasebattle.com/nobase_opengraph.png',
+        url: "https://nobasebattle.com/nobase_opengraph.png",
         width: 1200,
         height: 630,
-        alt: '무근본 배틀',
+        alt: "무근본 배틀",
       },
     ],
-    siteName: '무근본 배틀',
-    type: 'website',
+    siteName: "무근본 배틀",
+    type: "website",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="ko" suppressHydrationWarning>
@@ -41,9 +42,13 @@ export default function RootLayout({
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon-apple.png" />
       </Head>
+      {process.env.NODE_ENV === "production" &&
+        process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
+        )}
       <body
         className={
-          'flex flex-col items-center bg-white transition-colors duration-300 px-6 py-20 min-w-[330px] min-h-svh dark:bg-gray-700 text-gray-800 dark:text-gray-200 antialiased'
+          "flex flex-col items-center bg-white transition-colors duration-300 px-6 py-20 min-w-[330px] min-h-svh dark:bg-gray-700 text-gray-800 dark:text-gray-200 antialiased"
         }
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
@@ -53,5 +58,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
